79b7517cd455bc70b179a83acd84d67c97b807c1
Алексей Макаров <raminov.83@mail.ru>
2025-01-27 13:14:00 +0300

    мобильная верстка header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React , { Component } from "react"
-import "./Header.css";
+import style from "./Header.module.css"
 import userIcon from '../../assets/icons/user.svg';
 import searchIcon from '../../assets/icons/search.svg';
 import shopBagIcon from '../../assets/icons/shop-bag.svg';
@@ -8,21 +8,21 @@ import shopBagIcon from '../../assets/icons/shop-bag.svg';
 class Header extends Component {  
     render () {
     return (
-        <div className="header">
-            <div className="header-items">
-                <div className="header-wrapper">
-                    <p className="Header-logo">Bookshop</p>
-                <div className="Header-navigation">
-                    <a href="/some/valid/uri" style={{color: '#1C2A39'}}>BOOKS</a> 
-                    <a href="/some/valid/uri">AUDIOBOOKS</a>
-                    <a href="/some/valid/uri">STATIONERY & GIFTS</a>
-                    <a href="/some/valid/uri">BLOG</a>
-                </div>
-                <div className="header-icons">
-                    <button className="icon-user"><img src={userIcon} alt="user" /></button>
-                    <button className="icon-search"><img src={searchIcon} alt="user" /></button>
-                    <button className="icon-shop"><img src={shopBagIcon} alt="user" /></button>
-                </div>
+        <div className={style.header}>
+            <div className={style.header_items}>
+                <div className={style.header_wrapper}>
+                    <p className={style.header_logo}>Bookshop</p>
+                    <div className={style.header_navigation}>
+                        <a href="/some/valid/uri" style={{color: '#1C2A39'}}>BOOKS</a> 
+                        <a href="/some/valid/uri">AUDIOBOOKS</a>
+                        <a href="/some/valid/uri">STATIONERY & GIFTS</a>
+                        <a href="/some/valid/uri">BLOG</a>
+                    </div>
+                    <div className={style.header_icons}>
+                        <button className="icon-user"><img src={userIcon} alt="user" /></button>
+                        <button className="icon-search"><img src={searchIcon} alt="user" /></button>
+                        <button className="icon-shop"><img src={shopBagIcon} alt="user" /></button>
+                    </div>
                 </div>
                 
             </div>
